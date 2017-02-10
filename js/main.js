4dd5b13ba11569b909a9dab00972832d0bc3f766
Martín González <m@martingonzalez.net>
2017-02-10 18:15:54 +0100

    Smooth scrolling to anchor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+/* Smooth scroll on anchor */
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 300);
+        return false;
+      }
+    }
+  });
+});
+
+/* Stick header */
 var sticky = new Waypoint.Sticky({
   element: $('.header')[0],
 })
