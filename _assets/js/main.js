64f4f64195c1978a6c2703514a5fa427f4a7b4e5
Martín González <m@martingonzalez.net>
2017-02-24 15:57:27 +0100

    Implement projects grid and popup

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,24 +1,57 @@
 //= require vendor/jquery-3.1.1.min.js
 //= require vendor/jquery.waypoints.min.js
 //= require vendor/sticky.min.js
+//= require vendor/jquery.magnific-popup.min.js
+//= require vendor/packery.pkgd.min.js
+//= require vendor/imagesloaded.pkgd.min.js
 
-/* Smooth scroll on anchor */
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 100
-        }, 300);
-        return false;
+$(document).ready(function() {
+  /* Smooth scroll to anchor */
+  $(function() {
+    $('.header a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top - 100
+          }, 300);
+          return false;
+        }
       }
-    }
+    });
+  });
+  
+  /* Stick header */
+  var sticky = new Waypoint.Sticky({
+    element: $('.section_home .header')[0]
+  });
+
+	$('.project-gallery').magnificPopup({
+		type: 'inline',
+    delegate: 'a',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+  
+  /* Build project grid */
+  var $grid = $('.project-gallery').imagesLoaded(function() {
+    $grid.packery({
+      // options
+      itemSelector: '.project',
+      gutter: 0,
+      percentPosition: true
+    });
   });
 });
 
-/* Stick header */
-var sticky = new Waypoint.Sticky({
-  element: $('.header')[0],
-})
