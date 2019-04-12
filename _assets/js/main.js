a5e5936eb470ae0c585d655660639f67251c9f14
Alvaro Ortiz <alvaro@furilo.com>
2019-04-12 16:08:35 +0200

    Landing Populate Data initial

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -156,4 +156,30 @@ $(document).ready(function() {
     autoplaySpeed: 2000,
   });
 
+
+
+
+  // Populate Data Browser
+  // ToDo: Extract this code to its own files/repository
+
+  $(".pdb_data_list_series a").click(function(e) {
+    e.preventDefault();
+
+    $('.pdb_dataset_series').removeClass('slide-out-left');
+    $('.p_data_browser').removeClass('slide-in-left');
+
+    $('.p_data_browser').addClass('slide-out-left');
+    $('.pdb_dataset_series').addClass('slide-in-left');
+  });
+
+  $('.back_to_browse').click(function(e) {
+    e.preventDefault();
+    $('.p_data_browser').removeClass('slide-out-left');
+    $('.pdb_dataset_series').removeClass('slide-in-left');
+
+    $('.pdb_dataset_series').addClass('slide-out-left');
+    $('.p_data_browser').addClass('slide-in-left');
+  });
+
+
 });
