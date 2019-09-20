6351d9906aaa8b72fa40d6d9cea3e48d208bbcb2
Jorge Aznar <jorgeatgu@gmail.com>
2019-09-20 09:26:15 +0200

    Filters over articles not over sections | styles | checkbox

--- src/json/data.js
+++ src/json/data.js
@@ -1,104 +1,82 @@
 const data = {
-  "1": {
-    "title": "Preámbulo",
-    "subtitle": null,
-    "articles": [
-      {
-        "title": null,
-        "text": [
-          "La Nación española, deseando establecer la justicia, la libertad y la seguridad y promover el bien de cuantos la integran, en uso de su soberanía, proclama su voluntad de:",
-          "Garantizar la convivencia democrática dentro de la Constitución y de las leyes conforme a un orden económico y social justo.",
-          "Consolidar un Estado de Derecho que asegure el imperio de la ley como expresión de la voluntad popular.",
-          "Proteger a todos los españoles y pueblos de España en el ejercicio de los derechos humanos, sus culturas y tradiciones, lenguas e instituciones.",
-          "Promover el progreso de la cultura y de la economía para asegurar a todos una digna calidad de vida.",
-          "Establecer una sociedad democrática avanzada, y",
-          "Colaborar en el fortalecimiento de unas relaciones pacíficas y de eficaz cooperación entre todos los pueblos de la Tierra.",
-          "En consecuencia, las Cortes aprueban y el pueblo español ratifica la siguiente"
-        ]
-      }
-    ]
-  },
-  "2": {
-    "title": "Constitución",
-    "subtitle": "Título Preliminar",
-    "articles": [
-      {
+    "1":
+    {
+        "title": "Preámbulo",
+        "index": 1,
+        "articles": [
+        {
+            "title": null,
+            "text": [
+                "La Nación española, deseando establecer la justicia, la libertad y la seguridad y promover el bien de cuantos la integran, en uso de su soberanía, proclama su voluntad de:",
+                "Garantizar la convivencia democrática dentro de la Constitución y de las leyes conforme a un orden económico y social justo.",
+                "Consolidar un Estado de Derecho que asegure el imperio de la ley como expresión de la voluntad popular.",
+                "Proteger a todos los españoles y pueblos de España en el ejercicio de los derechos humanos, sus culturas y tradiciones, lenguas e instituciones.",
+                "Promover el progreso de la cultura y de la economía para asegurar a todos una digna calidad de vida.",
+                "Establecer una sociedad democrática avanzada, y",
+                "Colaborar en el fortalecimiento de unas relaciones pacíficas y de eficaz cooperación entre todos los pueblos de la Tierra.",
+                "En consecuencia, las Cortes aprueban y el pueblo español ratifica la siguiente"
+            ]
+        }]
+    },
+    "2":
+    {
+        "title": "Constitución",
+        "subtitle": "Título Preliminar",
+        "index": 1,
+        "articles": [
+        {
+            "text": [
+                "La Nación española, deseando establecer la justicia, la libertad y la seguridad y promover el bien de cuantos la integran, en uso de su soberanía, proclama su voluntad de:",
+                "Garantizar la convivencia democrática dentro de la Constitución y de las leyes conforme a un orden económico y social justo.",
+                "Consolidar un Estado de Derecho que asegure el imperio de la ley como expresión de la voluntad popular.",
+                "Proteger a todos los españoles y pueblos de España en el ejercicio de los derechos humanos, sus culturas y tradiciones, lenguas e instituciones.",
+                "Promover el progreso de la cultura y de la economía para asegurar a todos una digna calidad de vida.",
+                "Establecer una sociedad democrática avanzada, y",
+                "Colaborar en el fortalecimiento de unas relaciones pacíficas y de eficaz cooperación entre todos los pueblos de la Tierra.",
+                "En consecuencia, las Cortes aprueban y el pueblo español ratifica la siguiente"
+            ]
+        }]
+    },
+    "3":
+    {
         "title": "Artículo 1",
-        "text": [
-          "1. España se constituye en un Estado social y democrático de Derecho, que propugna como valores superiores de su ordenamiento jurídico la libertad, la justicia, la igualdad y el pluralismo político.",
-          "2. La soberanía nacional reside en el pueblo español, del que emanan los poderes del Estado.",
-          "3. La forma política del Estado español es la Monarquía parlamentaria."
-        ]
-      },
-      {
+        "index": 1,
+        "articles": [
+        {
+            "text": [
+                "La Nación española, deseando establecer la justicia, la libertad y la seguridad y promover el bien de cuantos la integran, en uso de su soberanía, proclama su voluntad de:",
+                "Garantizar la convivencia democrática dentro de la Constitución y de las leyes conforme a un orden económico y social justo.",
+                "Consolidar un Estado de Derecho que asegure el imperio de la ley como expresión de la voluntad popular.",
+                "Proteger a todos los españoles y pueblos de España en el ejercicio de los derechos humanos, sus culturas y tradiciones, lenguas e instituciones.",
+                "Promover el progreso de la cultura y de la economía para asegurar a todos una digna calidad de vida.",
+                "Establecer una sociedad democrática avanzada, y",
+                "Colaborar en el fortalecimiento de unas relaciones pacíficas y de eficaz cooperación entre todos los pueblos de la Tierra.",
+                "En consecuencia, las Cortes aprueban y el pueblo español ratifica la siguiente"
+            ]
+        }]
+    },
+    "4":
+    {
         "title": "Artículo 2",
-        "text": [
-          "La Constitución se fundamenta en la indisoluble unidad de la Nación española, patria común e indivisible de todos los españoles, y reconoce y garantiza el derecho a la autonomía de las nacionalidades y regiones que la integran y la solidaridad entre todas ellas."
-        ]
-      },
-      {
-        "title": "Artículo 3",
-        "text": [
-          "1. El castellano es la lengua española oficial del Estado. Todos los españoles tienen el deber de conocerla y el derecho a usarla.",
-          "2. Las demás lenguas españolas serán también oficiales en las respectivas Comunidades Autónomas de acuerdo con sus Estatutos.",
-          "3. La riqueza de las distintas modalidades lingüísticas de España es un patrimonio cultural que será objeto de especial respeto y protección."
-        ]
-      },
-      {
-        "title": "Artículo 4",
-        "text": [
-          "1. La bandera de España está formada por tres franjas horizontales, roja, amarilla y roja, siendo la amarilla de doble anchura que cada una de las rojas.",
-          "2. Los Estatutos podrán reconocer banderas y enseñas propias de las Comunidades Autónomas. Estas se utilizarán junto a la bandera de España en sus edificios públicos y en sus actos oficiales."
-        ]
-      },
-      {
+        "index": 1,
+        "articles": [
+        {
+            "text": [
+                "La Constitución se fundamenta en la indisoluble unidad de la Nación española, patria común e indivisible de todos los españoles, y reconoce y garantiza el derecho a la autonomía de las nacionalidades y regiones que la integran y la solidaridad entre todas ellas."
+            ]
+        }]
+    },
+    "5":
+    {
         "title": "Artículo 5",
-        "text": [
-          "La capital del Estado es la villa de Madrid."
-        ]
-      },
-      {
-        "title": "Artículo 6",
-        "text": [
-          "Los partidos políticos expresan el pluralismo político, concurren a la formación y manifestación de la voluntad popular y son instrumento fundamental para la participación política. Su creación y el ejercicio de su actividad son libres dentro del respeto a la Constitución y a la ley. Su estructura interna y funcionamiento deberán ser democráticos."
-        ]
-      },
-      {
-        "title": "Artículo 7",
-        "text": [
-          "Los sindicatos de trabajadores y las asociaciones empresariales contribuyen a la defensa y promoción de los intereses económicos y sociales que les son propios. Su creación y el ejercicio de su actividad son libres dentro del respeto a la Constitución y a la ley. Su estructura interna y funcionamiento deberán ser democráticos."
-        ]
-      },
-      {
-        "title": "Artículo 8",
-        "text": [
-          "1. Las Fuerzas Armadas, constituidas por el Ejército de Tierra, la Armada y el Ejército del Aire, tienen como misión garantizar la soberanía e independencia de España, defender su integridad territorial y el ordenamiento constitucional.",
-          "2. Una ley orgánica regulará las bases de la organización militar conforme a los principios de la presente Constitución."
-        ]
-      },
-      {
-        "title": "Artículo 9",
-        "text": [
-          "1. Los ciudadanos y los poderes públicos están sujetos a la Constitución y al resto del ordenamiento jurídico.",
-          "2. Corresponde a los poderes públicos promover las condiciones para que la libertad y la igualdad del individuo y de los grupos en que se integra sean reales y efectivas; remover los obstáculos que impidan o dificulten su plenitud y facilitar la participación de todos los ciudadanos en la vida política, económica, cultural y social.",
-          "3. La Constitución garantiza el principio de legalidad, la jerarquía normativa, la publicidad de las normas, la irretroactividad de las disposiciones sancionadoras no favorables o restrictivas de derechos individuales, la seguridad jurídica, la responsabilidad y la interdicción de la arbitrariedad de los poderes públicos."
-        ]
-      }
-    ]
-  },
-  "3": {
-    "title": "Título 1",
-    "subtitle": "De los derechos y deberes fundamentales",
-    "articles": [
-      {
-        "title": "Artículo 10",
-        "text": [
-          "1. La dignidad de la persona, los derechos inviolables que le son inherentes, el libre desarrollo de la personalidad, el respeto a la ley y a los derechos de los demás son fundamento del orden político y de la paz social.",
-          "2. Las normas relativas a los derechos fundamentales y a las libertades que la Constitución reconoce se interpretarán de conformidad con la Declaración Universal de Derechos Humanos y los tratados y acuerdos internacionales sobre las mismas materias ratificados por España."
-        ]
-      }
-    ]
-  }
+        "index": 1,
+        "articles": [
+        {
+            "text": [
+                "La capital del Estado es la villa de Madrid."
+            ]
+        }]
+    }
 }
 
 export default data;
