4f3f68caa0a17062ff521d304886409953aef744
Jorge Aznar <jorgeatgu@gmail.com>
2019-09-26 09:30:24 +0200

    remove package | add bold to index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
        <article class="browser">
-           <div class="browser-chapter-container" v-for="(item, index) in items" :class="item.scroll">
+           <div class="browser-chapter-container" v-for="(item, index) in items" :id="'browser-title-' + index">
                 <a
                     v-scroll-to="{el: '#seccion_' + index, container: '#constitution-text' }"
                     :class="item.class"
@@ -12,21 +12,23 @@
                        <span class="browser-chapter-subtitle" :class="item.class" >{{item.subtitle}}</span>
                    </div>
                 </a>
-                <div :id="'indice_' + index" class="browser-chapter-sections">
-                    <div class="browser-chapter-sections-element" v-for="(chapters, index) in item.articles">
-                        <a
-                            v-scroll-to="{el: '#seccion_' + chapters.index, container: '#constitution-text' }"
-                            :class="chapters.class"
-                            >
-                           <div class="browser-chapter-columns browser-chapter-columns-left">
-                               <span class="browser-chapter-title">{{chapters.title}}</span >
-                           </div>
-                           <div class="browser-chapter-columns">
-                               <span class="browser-chapter-subtitle" :class="chapters.class" >{{chapters.subtitle}}</span>
-                           </div>
-                        </a>
+                <transition name="fade" mode="out-in" appear>
+                    <div :id="'indice_' + index" class="browser-chapter-sections">
+                        <div class="browser-chapter-sections-element" v-for="(chapters, index) in item.articles">
+                            <a
+                                v-scroll-to="{el: '#seccion_' + chapters.index, container: '#constitution-text' }"
+                                :class="chapters.class"
+                                >
+                               <div class="browser-chapter-columns browser-chapter-columns-left">
+                                   <span class="browser-chapter-title">{{chapters.title}}</span >
+                               </div>
+                               <div class="browser-chapter-columns">
+                                   <span class="browser-chapter-subtitle" :class="chapters.class" >{{chapters.subtitle}}</span>
+                               </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                </transition>
            </div>
        </article>
 </template>
@@ -43,43 +45,42 @@ export default {
     },
     methods: {
         updateScroll() {
-            console.log('scoll')
             enterView({
                 selector: '#seccion_12',
                 enter: function() {
-                    const element = document.getElementById('indice_12')
+                    const element = document.getElementById('browser-title-12')
                     element.classList.add('active-index');
                 },
                 exit: function() {
-                    const element = document.getElementById('indice_12')
+                    const element = document.getElementById('browser-title-12')
                     element.classList.remove('active-index');
                 }
             });
             enterView({
                 selector: '#seccion_77',
                 enter: function() {
-                    const element = document.getElementById('indice_77')
+                    const element = document.getElementById('browser-title-77')
                     element.classList.add('active-index');
 
-                    const PreviousElement = document.getElementById('indice_12')
+                    const PreviousElement = document.getElementById('browser-title-12')
                     PreviousElement.classList.remove('active-index');
                 },
                 exit: function() {
-                    const element = document.getElementById('indice_77')
+                    const element = document.getElementById('browser-title-77')
                     element.classList.remove('active-index');
                 }
             });
             enterView({
                 selector: '#seccion_156',
                 enter: function() {
-                    const element = document.getElementById('indice_156')
+                    const element = document.getElementById('browser-title-156')
                     element.classList.add('active-index');
 
-                    const PreviousElement = document.getElementById('indice_77')
+                    const PreviousElement = document.getElementById('browser-title-77')
                     PreviousElement.classList.remove('active-index');
                 },
                 exit: function() {
-                    const element = document.getElementById('indice_156')
+                    const element = document.getElementById('browser-title-156')
                     element.classList.remove('active-index');
                 }
             });
@@ -87,7 +88,6 @@ export default {
         }
     },
     mounted() {
-        console.log('scroll')
         const browserCenter = document.getElementById('constitution-text')
         browserCenter.addEventListener('scroll', this.updateScroll);
     }
